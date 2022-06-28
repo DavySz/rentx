de477c910f5aa820286bc0fc35115a6d7b247a1e
davy <davydesouzabar@gmail.com>
2022-06-28 19:42:09 -0400

    fix: card car in home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,9 +3,28 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
 export function Home() {
+    const carDataOne = {
+        brand: "Audi",
+        name: "RS 5 Coupé",
+        rent: {
+            period: "Ao dia",
+            price: 120,
+        },
+        thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
+    };
+    const carDataTwo = {
+        brand: "Audi",
+        name: "RS 5 Coupé",
+        rent: {
+            period: "Ao dia",
+            price: 120,
+        },
+        thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
+    };
     return (
         <Container>
             <StatusBar
@@ -19,6 +38,8 @@ export function Home() {
                     <TotalCars>Total de 12 carros</TotalCars>
                 </HeaderContent>
             </Header>
+            <Car data={carDataOne} />
+            <Car data={carDataTwo} />
         </Container>
     );
 }
