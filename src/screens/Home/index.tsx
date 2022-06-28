7185102e35d7c886386b86af13939dd16c68fc83
davy <davydesouzabar@gmail.com>
2022-06-28 19:55:49 -0400

    refactor: list cards with  flatlist

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,19 +4,10 @@ import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
 import { Car } from "../../components/Car";
-import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
 
 export function Home() {
-    const carDataOne = {
-        brand: "Audi",
-        name: "RS 5 Coupé",
-        rent: {
-            period: "Ao dia",
-            price: 120,
-        },
-        thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
-    };
-    const carDataTwo = {
+    const carData = {
         brand: "Audi",
         name: "RS 5 Coupé",
         rent: {
@@ -38,8 +29,11 @@ export function Home() {
                     <TotalCars>Total de 12 carros</TotalCars>
                 </HeaderContent>
             </Header>
-            <Car data={carDataOne} />
-            <Car data={carDataTwo} />
+            <CarList
+                data={[1, 2, 3, 4, 5, 6, 7]}
+                keyExtractor={(item) => String(item)}
+                renderItem={({ item }) => <Car data={carData} />}
+            />
         </Container>
     );
 }
