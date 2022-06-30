ac2f1e50c1d775bb8363de480b95a303bd9330a7
davy <davydesouzabar@gmail.com>
2022-06-30 13:17:02 -0400

    feat: use stack navigation

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import * as SplashScreen from "expo-splash-screen";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 
-import { SchedulingComplete } from "./src/screens/SchedulingComplete";
+import { Routes } from "./src/routes";
 import theme from "./src/styles/theme";
 
 export default function App() {
@@ -33,7 +33,7 @@ export default function App() {
 
     return (
         <ThemeProvider theme={theme}>
-            <SchedulingComplete />
+            <Routes />
         </ThemeProvider>
     );
 }
