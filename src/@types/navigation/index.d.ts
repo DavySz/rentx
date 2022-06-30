379fc84fc77d3fb80d8f6652e141b60bbb1a8d0d
davy <davydesouzabar@gmail.com>
2022-06-30 13:17:24 -0400

    fix: route types

---
+++ src/@types/navigation/index.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            Home: undefined;
+            CarDetails: undefined;
+            Scheduling: undefined;
+            SchedulingDetails: undefined;
+            SchedulingComplete: undefined;
+        }
+    }
+}
