3ee1a0bfd9ba1f99dc2584feb770f19a0ebcb259
davy <davydesouzabar@gmail.com>
2022-06-30 19:29:32 -0400

    fix: route param type

--- src/@types/navigation/index.d.ts
+++ src/@types/navigation/index.d.ts
@@ -1,9 +1,13 @@
+import { CarDTO } from "../../dtos/CarDTO";
+
 /* eslint-disable @typescript-eslint/naming-convention */
 export declare global {
     namespace ReactNavigation {
         interface RootParamList {
             Home: undefined;
-            CarDetails: undefined;
+            CarDetails: {
+                car: CarDTO;
+            };
             Scheduling: undefined;
             SchedulingDetails: undefined;
             SchedulingComplete: undefined;
