865eafd46e6e5f297e326edefd7c1b8075aa6e24
davy <davydesouzabar@gmail.com>
2022-07-05 19:50:36 -0400

    feat: create float absolute button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { useTheme } from "styled-components/native";
 
 import Logo from "../../assets/logo.svg";
 import { Car } from "../../components/Car";
 import { Loading } from "../../components/Loading";
 import { CarDTO } from "../../dtos/CarDTO";
 import api from "../../services/api";
-import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
+import {
+    Container,
+    Header,
+    TotalCars,
+    HeaderContent,
+    CarList,
+    MyCarsButton,
+} from "./styles";
 
 export function Home() {
     const [isLoading, setIsLoading] = useState(true);
     const [cars, setCars] = useState<CarDTO[]>([]);
     const navigation = useNavigation();
+
+    const theme = useTheme();
 
     async function fetchCars() {
         try {
@@ -32,6 +44,10 @@ export function Home() {
 
     function handleCarDetails(car: CarDTO) {
         navigation.navigate("CarDetails", { car });
+    }
+
+    function handleOpenMyCars() {
+        navigation.navigate("MyCars");
     }
 
     return (
@@ -61,6 +77,13 @@ export function Home() {
                     )}
                 />
             )}
+            <MyCarsButton onPress={() => handleOpenMyCars()}>
+                <Ionicons
+                    name="ios-car-sport"
+                    size={32}
+                    color={theme.colors.shape}
+                />
+            </MyCarsButton>
         </Container>
     );
 }
