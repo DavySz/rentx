89727d98e3e9a7e92ecfcef328fe5358c93b8be3
davy <davydesouzabar@gmail.com>
2022-07-05 20:32:58 -0400

    feat: add disable and loading state

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
+import { ActivityIndicator } from "react-native";
+import { useTheme } from "styled-components";
 
 import { Container, Title } from "./styles";
 
@@ -6,12 +10,30 @@ type Props = {
     type: "primary" | "secondary";
     title: string;
     onPress: () => void;
+    disabled?: boolean;
+    loading?: boolean;
 };
 
-export function Button({ type, title, onPress }: Props) {
+export function Button({
+    type,
+    title,
+    onPress,
+    disabled = false,
+    loading = false,
+}: Props) {
+    const theme = useTheme();
     return (
-        <Container type={type} onPress={onPress}>
-            <Title>{title}</Title>
+        <Container
+            type={type}
+            onPress={onPress}
+            disabled={disabled}
+            style={{ opacity: disabled === true || loading === true ? 0.5 : 1 }}
+        >
+            {loading ? (
+                <ActivityIndicator color={theme.colors.shape} />
+            ) : (
+                <Title>{title}</Title>
+            )}
         </Container>
     );
 }
