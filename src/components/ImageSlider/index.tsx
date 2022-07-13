db6945b474154ea47b77c663f90b8ffb6eb8237b
davy <davydesouzabar@gmail.com>
2022-07-13 18:26:20 -0400

    feat: add car slider

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-array-index-key */
-import React from "react";
-import { FlatList } from "react-native";
+import React, { useRef, useState } from "react";
+import { FlatList, ViewToken } from "react-native";
 
 import {
     Container,
@@ -14,12 +14,26 @@ type Props = {
     imagesUrl: string[];
 };
 
+type ChangeImageProps = {
+    viewableItems: ViewToken[];
+    changed: ViewToken[];
+};
+
 export function ImageSlider({ imagesUrl }: Props) {
+    const [imageIndex, setImageIndex] = useState(0);
+    const indexChanged = useRef((info: ChangeImageProps) => {
+        const { index } = info.viewableItems[0];
+        setImageIndex(index);
+    });
+
     return (
         <Container>
             <ImageIndexs>
                 {imagesUrl.map((_, index) => (
-                    <ImageIndex active key={index.toString()} />
+                    <ImageIndex
+                        active={index === imageIndex}
+                        key={index.toString()}
+                    />
                 ))}
             </ImageIndexs>
             <FlatList
@@ -32,6 +46,8 @@ export function ImageSlider({ imagesUrl }: Props) {
                 )}
                 horizontal
                 showsHorizontalScrollIndicator={false}
+                onViewableItemsChanged={indexChanged.current}
+                pagingEnabled
             />
         </Container>
     );
