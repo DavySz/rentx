ab87183a7cf734d968fc938174423a91531f5665
davy <davydesouzabar@gmail.com>
2022-07-17 13:17:51 -0400

    feat: add button animation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 /* eslint-disable import/no-extraneous-dependencies */
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, StyleSheet, TouchableOpacity } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    useAnimatedGestureHandler,
+    withSpring,
+} from "react-native-reanimated";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useTheme } from "styled-components/native";
 
@@ -11,19 +19,41 @@ import { Car } from "../../components/Car";
 import { Loading } from "../../components/Loading";
 import { CarDTO } from "../../dtos/CarDTO";
 import api from "../../services/api";
-import {
-    Container,
-    Header,
-    TotalCars,
-    HeaderContent,
-    CarList,
-    MyCarsButton,
-} from "./styles";
+import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
+
+const ButtonAnimated = Animated.createAnimatedComponent(TouchableOpacity);
 
 export function Home() {
     const [isLoading, setIsLoading] = useState(true);
     const [cars, setCars] = useState<CarDTO[]>([]);
     const navigation = useNavigation();
+
+    const positionY = useSharedValue(0);
+    const positionX = useSharedValue(0);
+
+    const myCarsButtonStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                { translateX: positionX.value },
+                { translateY: positionY.value },
+            ],
+        };
+    });
+
+    const onGestureEvent = useAnimatedGestureHandler({
+        onStart(_, ctx: any) {
+            ctx.positionX = positionX.value;
+            ctx.positionY = positionY.value;
+        },
+        onActive(event, ctx) {
+            positionX.value = ctx.positionX + event.translationX;
+            positionY.value = ctx.positionY + event.translationY;
+        },
+        onEnd() {
+            positionX.value = withSpring(0);
+            positionY.value = withSpring(0);
+        },
+    });
 
     const theme = useTheme();
 
@@ -77,13 +107,43 @@ export function Home() {
                     )}
                 />
             )}
-            <MyCarsButton onPress={() => handleOpenMyCars()}>
-                <Ionicons
-                    name="ios-car-sport"
-                    size={32}
-                    color={theme.colors.shape}
-                />
-            </MyCarsButton>
+            <PanGestureHandler onGestureEvent={onGestureEvent}>
+                <Animated.View
+                    style={[
+                        myCarsButtonStyle,
+                        {
+                            position: "absolute",
+                            bottom: 13,
+                            right: 22,
+                        },
+                    ]}
+                >
+                    <ButtonAnimated
+                        onPress={() => handleOpenMyCars()}
+                        style={[
+                            styles.button,
+                            { backgroundColor: theme.colors.main },
+                        ]}
+                        activeOpacity={0.7}
+                    >
+                        <Ionicons
+                            name="ios-car-sport"
+                            size={32}
+                            color={theme.colors.shape}
+                        />
+                    </ButtonAnimated>
+                </Animated.View>
+            </PanGestureHandler>
         </Container>
     );
 }
+
+const styles = StyleSheet.create({
+    button: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
