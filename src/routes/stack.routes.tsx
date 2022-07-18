dffd7b2d0f809054d4e204ad994abdfb4015b914
davy <davydesouzabar@gmail.com>
2022-07-18 09:59:33 -0400

    fix: signin route

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,6 +6,7 @@ import { MyCars } from "../screens/MyCars";
 import { Scheduling } from "../screens/Scheduling";
 import { SchedulingComplete } from "../screens/SchedulingComplete";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
+import { SignIn } from "../screens/SignIn";
 import { Splash } from "../screens/Splash";
 
 const { Navigator, Screen } = createNativeStackNavigator();
@@ -19,6 +20,7 @@ export function StackRoutes() {
             initialRouteName="Splash"
         >
             <Screen name="Splash" component={Splash} />
+            <Screen name="SignIn" component={SignIn} />
             <Screen
                 name="Home"
                 component={Home}
