ff415a6b1a5fba785e141e26f9a97e970232e69f
davy <davydesouzabar@gmail.com>
2022-07-19 08:40:22 -0400

    feat: create signin screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React from "react";
+import { StatusBar } from "react-native";
+import { useTheme } from "styled-components";
+
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { InputPassword } from "../../components/InputPassword";
+import { Container, Header, Title, Subtitle, Form, Footer } from "./styles";
+
+export function SignIn() {
+    const theme = useTheme();
+    return (
+        <Container>
+            <StatusBar
+                backgroundColor="transparent"
+                barStyle="dark-content"
+                translucent
+            />
+            <Header>
+                <Title>Estamos{"\n"}quase lá.</Title>
+                <Subtitle>
+                    Faça seu login para começar{"\n"}uma experiência incrível.
+                </Subtitle>
+            </Header>
+            <Form>
+                <Input
+                    iconName="mail"
+                    autoCorrect={false}
+                    placeholder="E-mail"
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                />
+                <InputPassword iconName="lock" placeholder="Senha" />
+            </Form>
+            <Footer>
+                <Button
+                    title="Login"
+                    onPress={() => {}}
+                    disabled
+                    loading={false}
+                />
+                <Button
+                    color={theme.colors.background_secondary}
+                    title="Criar conta gratuita"
+                    onPress={() => {}}
+                    disabled
+                    loading={false}
+                    light
+                />
+            </Footer>
+        </Container>
+    );
+}
