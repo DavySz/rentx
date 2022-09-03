4af8ce234ffb51ab18af2f7378ba13fbda7891aa
davy <davydesouzabar@gmail.com>
2022-09-03 18:11:41 -0400

    feat: create bullet compoent

---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/require-default-props */
+import React from "react";
+
+import { Container } from "./styles";
+
+interface IBulletProps {
+    active?: boolean;
+}
+
+export function Bullet({ active = false }: IBulletProps) {
+    return <Container active={active} />;
+}
