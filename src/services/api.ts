998e38ccca650320e721127c99b40ec0e224d76d
davy <davydesouzabar@gmail.com>
2022-09-04 14:02:44 -0400

    refactor: using url env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
+/* eslint-disable import/no-unresolved */
+import { API_URL } from "@env";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://192.168.0.23:3333",
+    baseURL: API_URL,
 });
 
 export default api;
