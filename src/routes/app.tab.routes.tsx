345ba49c4dc58a0c85400437064d097f02f2ae42
davy <davydesouzabar@gmail.com>
2022-09-03 21:54:26 -0400

    feat: create auth and public routes

---
+++ src/routes/app.tab.routes.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable react/no-unstable-nested-components */
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Platform } from "react-native";
+import { useTheme } from "styled-components";
+
+import CarSvg from "../assets/car.svg";
+import HomeSvg from "../assets/home.svg";
+import PeopleSvg from "../assets/people.svg";
+import { Home } from "../screens/Home";
+import { MyCars } from "../screens/MyCars";
+import { AppStackRoutes } from "./app.stack.routes";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppTabRoutes() {
+    const theme = useTheme();
+    return (
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: theme.colors.main,
+                tabBarInactiveTintColor: theme.colors.text_details,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    paddingVertical: Platform.OS === "ios" ? 20 : 0,
+                    height: 78,
+                    backgroundColor: theme.colors.background_primary,
+                },
+            }}
+        >
+            <Screen
+                name="Home"
+                component={AppStackRoutes}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <HomeSvg height={24} width={24} fill={color} />
+                    ),
+                }}
+            />
+            <Screen
+                name="Profile"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <PeopleSvg height={24} width={24} fill={color} />
+                    ),
+                }}
+            />
+            <Screen
+                name="MyCars"
+                component={MyCars}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <CarSvg height={24} width={24} fill={color} />
+                    ),
+                }}
+            />
+        </Navigator>
+    );
+}
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,11 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { CarDetails } from "../screens/CarDetails";
 import { Confirmation } from "../screens/Confirmation";
-import { Home } from "../screens/Home";
-import { MyCars } from "../screens/MyCars";
-import { Scheduling } from "../screens/Scheduling";
-import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { SignIn } from "../screens/SignIn";
 import { SignUpFirstStep } from "../screens/SignUp/SignUpFirstStep";
 import { SignUpSecondStep } from "../screens/SignUp/SignUpSecondStep";
@@ -13,7 +8,7 @@ import { Splash } from "../screens/Splash";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
-export function StackRoutes() {
+export function AuthRoutes() {
     return (
         <Navigator
             screenOptions={{
@@ -23,20 +18,9 @@ export function StackRoutes() {
         >
             <Screen name="Splash" component={Splash} />
             <Screen name="SignIn" component={SignIn} />
-            <Screen
-                name="Home"
-                component={Home}
-                options={{
-                    gestureEnabled: false,
-                }}
-            />
-            <Screen name="CarDetails" component={CarDetails} />
-            <Screen name="Scheduling" component={Scheduling} />
-            <Screen name="SchedulingDetails" component={SchedulingDetails} />
-            <Screen name="Confirmation" component={Confirmation} />
-            <Screen name="MyCars" component={MyCars} />
             <Screen name="SignUpFirstStep" component={SignUpFirstStep} />
             <Screen name="SignUpSecondStep" component={SignUpSecondStep} />
+            <Screen name="Confirmation" component={Confirmation} />
         </Navigator>
     );
 }
