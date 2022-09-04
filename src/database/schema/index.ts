76fc0ca9c188685d79348301a2d94e0d1e073265
davy <davydesouzabar@gmail.com>
2022-09-04 18:45:35 -0400

    feat: add car schema

--- src/database/schema/index.ts
+++ src/database/schema/index.ts
@@ -1,10 +1,11 @@
 import { appSchema } from "@nozbe/watermelondb";
 
+import { carSchema } from "./CarSchema";
 import { userSchema } from "./UserSchema";
 
 const schemas = appSchema({
-    version: 1,
-    tables: [userSchema],
+    version: 2,
+    tables: [userSchema, carSchema],
 });
 
 export { schemas };
