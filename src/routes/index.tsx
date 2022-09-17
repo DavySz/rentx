e464d6df94ee2addcf58ddee3591320ba530c168
davy <davydesouzabar@gmail.com>
2022-09-17 19:59:16 -0400

    feat: use validation loading state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 
+import { Loading } from "../components/Loading";
 import { useAuth } from "../hooks/auth";
 import { AppTabRoutes } from "./app.tab.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
-    const { user } = useAuth();
-    return (
+    const { user, loading } = useAuth();
+    return loading ? (
+        <Loading />
+    ) : (
         <NavigationContainer>
             {user.id ? <AppTabRoutes /> : <AuthRoutes />}
         </NavigationContainer>
